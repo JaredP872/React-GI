783ec2b0bf6d61a075d62753955267fd2da5b7ef
JaredP872 <jared.proctor7642@gmail.com>
2025-01-19 19:40:27 -0500

    added comments

--- my-react-gi/src/App.js
+++ my-react-gi/src/App.js
@@ -1,11 +1,17 @@
+// Imports the react library that we need in order to create and use react components.
 import React from "react";
+//Imports the basic Info components from the BasicInfo.js file.
 import BasicInfo from "./BasicInfo";
 
+// Declares a class based react component named App thet extends React.component. This is needed to manage this.state
 class App extends React.Component {
+  // Declares the construtor method for the App component used to initialize state and set up the component. The constructor is required to initialize the components this.state and super(props)
   constructor(props) {
+    // Calls the parent class(React.Component) constructor with props. This ensures that the props onject is correclty passed to the parent class, allowing this.props to work properly.
     super(props);
-
+    // Initializes the state object of the App component with a people array containing details of multiple peoples information. The state is where the components data is stored. The people array will be iterated over to render individual BasicInfo components.
     this.state = {
+      // stores an array of objects where each object represents a person with properties. Such as: FirstName, LastName, Phone, and DateOfBirth.
       people: [
         {
           FirstName: "Jared",
@@ -40,11 +46,16 @@ class App extends React.Component {
       ],
     };
   }
+  // Declares the render method which defines what the App component will display on the screen. The render method is required in every class based react component. It returns JSX (HTML like syntax for React)
   render() {
     return (
+      // The outer container for the components UI. React components need to return a single root element. THis div serves as that root element.
       <div>
+        {/* Displays the heading Contacts on the UI */}
         <h1>Contacts</h1>
+        {/* Iterates over the people array in this.state using the map(function). The steps this part of my code takes: Creates a BasicInfo component for each person, passes the person object as a prop and adds a key prop to uniquely identify each component in the list.*/}
         {this.state.people.map((person, index) => (
+          // Creates an instance of the BasicInfo component for each person in the people array. Then passes the person object as a prop to the component. Once thats complete the key prop is added for React to efficiently update the DOM when the list changes. This is needed to display each persons details using the resuable BasicInfo component.
           <BasicInfo key={index} person={person} />
         ))}
       </div>
@@ -52,4 +63,5 @@ class App extends React.Component {
   }
 }
 
+// exports the app component as the default export of this file.
 export default App;
--- my-react-gi/src/BasicInfo.js
+++ my-react-gi/src/BasicInfo.js
@@ -1,17 +1,26 @@
+// Imports the react library that we need in order to create and use react components.
 import React from "react";
+// Imports my BasicInfo.css file allowing me to style my components.
+import "./BasicInfo.css";
 
+// Declares a component called BasicInfo
 const BasicInfo = (props) => {
-  const { FirstName, LastName, phone, dateOfBirth } = props.person;
-
+  // I'm using destructoring in order to extract specific preoperties that I want from props.person.
+  const { FirstName, LastName, Phone, DateOfBirth } = props.person;
+  // returns JSX which defines the structure and content of the user interface for this component.
   return (
-    <div>
+    // The className allows me to style this component with my BasicInfo.css file. This div container also serves as the container for my Basic information content.
+    <div className="basic-info">
+      {/* Renders a h2 element on the UI that stores the text Basic information */}
       <h2>Basic information</h2>
+      {/* The p elements do the same as the h2 elements but for paragraphs instead of h2 */}
       <p>First name: {FirstName}</p>
       <p>Last name: {LastName}</p>
-      <p>Phone: {phone}</p>
-      <p>Date of birth: {dateOfBirth} </p>
+      <p>Phone: {Phone}</p>
+      <p>Date of birth: {DateOfBirth} </p>
     </div>
   );
 };
 
+// exports the BasicInfo component as the default export on this file. This makes the component reasuable and allows files to imprt it bu using: import BasicInfo from "./BasicInfo"
 export default BasicInfo;
